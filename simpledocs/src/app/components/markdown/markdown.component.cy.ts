0cb8996c7c59f31fc70cddb8150ac139e6ddf8d2
Khushi Joshi <khushi.joshi@simptel.in>
2024-01-04 17:28:20 +0530

    added markdown component

--- simpledocs/src/app/components/markdown/markdown.component.cy.ts
+++ simpledocs/src/app/components/markdown/markdown.component.cy.ts
@@ -1,3 +1,6 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MarkdownComponent } from './markdown.component'
 
 describe('MarkdownComponent', () => {
@@ -5,3 +8,22 @@ describe('MarkdownComponent', () => {
     cy.mount(MarkdownComponent)
   })
 })
+
+describe("MarkdownComponent Unit Test", () => {
+  let component: MarkdownComponent;
+  let fixture: ComponentFixture<MarkdownComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CommonModule, HttpClientTestingModule],
+    }).compileComponents();
+    fixture = TestBed.createComponent(MarkdownComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).to.equal(component);
+  });
+
+});
--- simpledocs/src/app/components/markdown/markdown.component.ts
+++ simpledocs/src/app/components/markdown/markdown.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-markdown',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './markdown.component.html',
 })
 export class MarkdownComponent {
+  @Input() documentPath!: string;
+  public renderedMarkdown$!: Observable<SafeHtml>;
 
+  constructor(private markedService: MarkedService) {
+    this.documentPath = this.documentPath ?? 'assets/legal/terms-and-conditions.md';
+  }
+
+  ngOnInit(): void {
+    this.renderedMarkdown$ = this.markedService.fetchAndRenderMarkdown(this.documentPath);
+    this.renderedMarkdown$.subscribe();
+  }
 }
