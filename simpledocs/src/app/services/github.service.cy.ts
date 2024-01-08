b13f8e8168cc31f45a9ee5ceccdbd8166ca19c39
Khushi Joshi <khushi.joshi@simptel.in>
2024-01-08 18:34:37 +0530

    updated unit tests

--- simpledocs/src/app/services/github.service.cy.ts
+++ simpledocs/src/app/services/github.service.cy.ts
@@ -1,3 +1,7 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { GithubService } from "./github.service";
+
 describe('should test github service', () => {
   it('should test github api', () => {
     cy.request('GET', 'https://api.github.com/repos/simptel/docs.simptel.com/contents/docs').as('docs');
@@ -6,4 +10,34 @@ describe('should test github service', () => {
     cy.request('GET', 'https://api.github.com/repos/simptel/docs.simptel.com/contents/docs/Getting Started').as('getting-started');
     cy.get('@getting-started').should((response) => { });
   });
+});
+
+describe('GithubService unit tests', () => {
+  let service: GithubService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(GithubService);
+  });
+
+  it('should test simpledocs api with repoName', () => {
+    let endpoint = `https://api.github.com/repos/simptel/docs.simptel.com/contents/docs`;
+    let data = service.showSimpleDocs(endpoint);
+    expect(data).to.be.an('Object');
+  });
+
+  it('should test simpledocs api with directoryName', () => {
+    let endpoint = `https://api.github.com/repos/simptel/docs.simptel.com/contents/docs/Introduction`;
+    let data = service.showSimpleDocs(endpoint);
+    expect(data).to.be.an('Object');
+  });
+
+  it('should test simpledocs api with subDirectoryName', () => {
+    let endpoint = `https://api.github.com/repos/simptel/docs.simptel.com/contents/docs/Introduction/Glossary`;
+    let data = service.showSimpleDocs(endpoint);
+    expect(data).to.be.an('Object');
+  });
+
 });
