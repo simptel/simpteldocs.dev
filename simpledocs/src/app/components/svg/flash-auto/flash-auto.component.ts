1e6837733dcc57ae3bd1da52ae874faafbcacb1e
Khushi Joshi <khushi.joshi@simptel.in>
2024-01-09 10:34:42 +0530

    svg icons

---
+++ simpledocs/src/app/components/svg/flash-auto/flash-auto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flash-auto-svg',
+  standalone: true,
+  imports: [],
+  templateUrl: './flash-auto.component.svg',
+})
+export class FlashAutoComponent {
+
+}
