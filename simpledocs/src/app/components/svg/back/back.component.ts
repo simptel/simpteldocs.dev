0a0fee2b796320149708257348de7d460249a441
Khushi Joshi <khushi.joshi@simptel.in>
2024-01-12 18:33:08 +0530

    added svg components

---
+++ simpledocs/src/app/components/svg/back/back.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-back-svg',
+  templateUrl: './back.component.svg',
+})
+export class BackSvgComponent {
+
+}
---
+++ simpledocs/src/app/components/svg/chevron-right/chevron-right.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-chevron-right-svg',
+  templateUrl: './chevron-right.component.svg',
+})
+export class ChevronRightSvgComponent {
+
+}
